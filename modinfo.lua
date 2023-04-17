2337b0637248f31c4309495f93dc22311f8d992e
kevin <7692819+kevindevm@users.noreply.github.com>
2023-04-17 17:01:39 -0300

    upload

---
+++ modinfo.lua
@@ -0,0 +1,71 @@
+name = 'Auto-Unequip on 1% (Dreadstone Update)'
+description =
+    'Client mod. Automatically unequips your magiluminescence upon reaching (1-10)% durability to prevent it from breaking. Feature also applies to similar equippables such as eyebrellas, puffy vests, rain hats, etc.\nalso udpate to An Eye for an Eye Update and Dreadstone set'
+author = 'Kevindevm / John Watson'
+version = '5.71'
+forumthread = ''
+api_version = 10
+dst_compatible = true
+client_only_mod = true
+dont_starve_compatible = false
+reign_of_giants_compatible = true
+all_clients_require_mod = false
+icon_atlas = 'icon.xml'
+icon = 'icon.tex'
+server_filter_tags = {}
+
+local boolean = {{
+    description = "Yes",
+    data = true
+}, {
+    description = "No",
+    data = false
+}}
+
+-- 210215 null: original BuildNumConfig() breaks on saving Double click speed for 0.15, 0.4, 0.45, and 0.5 values (they reset to 0)
+-- Created an alternative function to handle decimal step values
+-- Continue to use original BuildNumConfig() to maintain old functionality
+-- Use nullBuildNumConfig() when needing to use float step values
+local function nullBuildNumConfig(start_num, end_num, step, percent)
+    local num_table = {}
+    local iterator = 1
+    local suffix = percent and "%" or ""
+
+    local ostart_num, oend_num, ostep -- For storing original parameters if needed
+    if step > 0 and step < 1 then -- If step = float between 0 and 1 (IE, Double click speed)
+        ostart_num, oend_num, ostep = start_num, end_num, step -- Store the original parameters
+
+        -- Convert floats to integers (only 2 decimal places though)
+        start_num = start_num * 100
+        end_num = end_num * 100
+        step = step * 100
+    end
+
+    for i = start_num, end_num, step do -- if step was a non-integer, iterate as integers instead
+        local i = ostep and i / 100 or i -- if step was a non-integer, convert i back to a float first
+
+        num_table[iterator] = {
+            description = i .. suffix,
+            data = percent and i / 100 or i
+        } -- original code
+        iterator = iterator + 1
+    end
+    return num_table
+end
+local function AddConfig(label, name, options, default, hover)
+    return {
+        label = label,
+        name = name,
+        options = options,
+        default = default,
+        hover = hover or ""
+    }
+end
+
+configuration_options = {AddConfig("Eye Mask and Shield Of terror", "MAU_EYE", nullBuildNumConfig(1, 40, 1, "%"), 25),
+                         AddConfig("Dreadston Set", "MAU_DREADSTONE", nullBuildNumConfig(1, 40, 1, "%"), 5),
+                         AddConfig("Notify on unequip", "MAU_notif", boolean, true),
+                         AddConfig("Unequip At ", "MAU_unequipPer", nullBuildNumConfig(1, 10, 1, "%"), 1),
+                         AddConfig("Ignore hand slot items", "MAU_hands", boolean, true),
+                         AddConfig("Force retrying unequip", "MAU_force", boolean, true),
+                         AddConfig("Debug mode", "debug_mode", boolean, false)}
---
+++ modmain.lua
@@ -0,0 +1,241 @@
+local G = GLOBAL
+
+local cfgNotif = GetModConfigData('MAU_notif')
+local cfgForce = GetModConfigData('MAU_force')
+local cfgHands = GetModConfigData('MAU_hands')
+local cfgEyeItems = GetModConfigData('MAU_EYE')
+local cfgDreadstone = GetModConfigData('MAU_DREADSTONE')
+local cfgPercent = GetModConfigData('MAU_unequipPer')
+local debug_mode = GetModConfigData('debug_mode')
+
+if cfgEyeItems < 1 and cfgPercent ~= false then
+    cfgEyeItems = cfgEyeItems * 100
+end
+if cfgDreadstone < 1 and cfgPercent ~= false then
+    cfgDreadstone = cfgDreadstone * 100
+end
+
+if cfgPercent < 1 then
+    cfgPercent = cfgPercent * 100
+end
+
+local notification = '%s\nauto-unequipped'
+
+local EYEUPDATE = {
+    shieldofterror = true,
+    eyemaskhat = true
+
+}
+
+local DREADSTONE = {
+    armordreadstone = true,
+    dreadstonehat = true
+
+}
+
+local NONREFILLABLE = {
+    armor_sanity = true,
+    armordragonfly = true,
+    armorgrass = true,
+    armormarble = true,
+    armorruins = true,
+    armorskeleton = true,
+    armorsnurtleshell = true,
+    armorwood = true,
+    beehat = true,
+    blue_mushroomhat = true,
+    blueamulet = true,
+    bushhat = true,
+    flowerhat = true,
+    footballhat = true,
+    green_mushroomhat = true,
+    greenamulet = true,
+    hawaiianshirt = true,
+    minerhat = true,
+    onemanband = true,
+    orangeamulet = true,
+    purpleamulet = true,
+    red_mushroomhat = true,
+    ruinshat = true,
+    sansundertalehat = true,
+    slurper = true,
+    slurtlehat = true,
+    spiderhat = true,
+    watermelonhat = true,
+    wathgrithrhat = true,
+    -- VANILLA UNREPAIRABLES
+    armor_bramble = true,
+    cookiecutterhat = true,
+
+    -- MODDED UNREPAIRABLES
+    armor_rock = true,
+    gear_wings = true,
+    hat_marble = true,
+    hat_rock = true,
+
+    -- MODDED UNBREAKABLES
+    candlehat = true,
+    axe = true,
+    batbat = true,
+    brush = true,
+    glasscutter = true,
+    golden_farm_hoe = true,
+    goldenaxe = true,
+    goldenpickaxe = true,
+    goldenshovel = true,
+    hammer = true,
+    lighter = true,
+    bernie_inactive = true,
+    minifan = true,
+    malbatross_beak = true,
+    moonglassaxe = true,
+    multitool_axe_pickaxe = true,
+    nightstick = true,
+    nightsword = true,
+    oar = true,
+    oar_driftwood = true,
+    pitchfork = true,
+    ruins_bat = true,
+    spear = true,
+    spear_wathgrithr = true,
+    tentaclespike = true,
+    torch = true,
+    trident = true,
+    whip = true,
+
+    -- VANILLA UNBREAKABLES
+    lantern = true,
+    premiumwateringcan = true,
+    wateringcan = true,
+    pocketwatch_weapon = true,
+
+    -- MODDED UNREPAIRABLES
+    dark_axe = true,
+    dark_pickaxe = true,
+    fryingpan = true,
+    mace_sting = true,
+    sword_rock = true,
+
+    -- MODDED UNBREAKABLES
+    bottlelantern = true,
+    broomstick = true,
+    elegantlantern = true,
+    opulentlantern = true,
+    scythe = true,
+    snowglobe = true
+
+    -- MODDED Tropical Experience
+
+}
+
+local DebugPrint = debug_mode and function(...)
+    local msg = "[Auto Unequip At " .. cfgPercent .. "%]"
+    for i = 1, arg.n do
+        msg = msg .. " " .. tostring(arg[i])
+    end
+    print(msg)
+end or function() --[[disabled]]
+end
+
+local function Unequip(inst)
+
+    if inst.replica.equippable:IsEquipped() then
+        G.ThePlayer.replica.inventory:ControllerUseItemOnSelfFromInvTile(inst)
+    end
+
+    if cfgForce and not inst.replica.equippable:IsEquipped() and inst.unequiptask ~= nil then
+        inst.unequiptask:Cancel()
+        inst.unequiptask = nil
+    end
+
+end
+
+local function AutoUnequip(inst)
+
+    local item = inst.entity:GetParent()
+    local slot = item.replica.equippable:EquipSlot()
+    local Cur_Percent = inst.percentused:value()
+
+    if (not item.replica.inventoryitem:IsHeldBy(G.ThePlayer)) or (not item.replica.equippable:IsEquipped()) then
+        return
+    end
+    local shouldbe = false
+    local reason = "none"
+
+    if shouldbe == false then
+        shouldbe = not (cfgHands and slot ~= G.EQUIPSLOTS.HANDS and not NONREFILLABLE[item.prefab]) or false
+        reason = "WILL UNIQUIP HAND"
+    end
+    if shouldbe == false then
+        shouldbe = cfgEyeItems and EYEUPDATE[item.prefab] or false
+        reason = "(EYE) unequip at=" .. tostring(cfgEyeItems)
+    end
+    if shouldbe == false then
+        shouldbe = cfgDreadstone and DREADSTONE[item.prefab] or false
+        reason = "(DREADSTONE) unequip at=" .. tostring(cfgDreadstone)
+    end
+    if shouldbe == false then
+        shouldbe = not NONREFILLABLE[item.prefab] or false
+        reason = "NOT hardcode, unequip at=" .. tostring(cfgPercent)
+    end
+
+    DebugPrint("(1/3) i=" .. item.prefab .. " p=" .. Cur_Percent .. "% s=" .. slot)
+    DebugPrint("(2/3) should unequip? :" .. tostring(shouldbe) .. " reason=" .. reason)
+
+    DebugPrint("(3/3) EYE=" .. tostring(cfgEyeItems) .. "% DREAD=" .. tostring(cfgDreadstone) .. "% NORMAL=" ..
+                   tostring(cfgPercent) .. "%")
+
+    if (cfgHands and slot == G.EQUIPSLOTS.HANDS) or (NONREFILLABLE[item.prefab]) or
+        (cfgEyeItems and Cur_Percent > cfgEyeItems) or (cfgDreadstone and Cur_Percent > cfgDreadstone) or
+        (not cfgEyeItems and not cfgDreadstone and Cur_Percent > cfgPercent) then
+        return
+    end
+
+    -- DebugPrint("item.prefab=" .. item.prefab ac_resend.. " at=" .. Cur_Percent .. "% slot=" .. slot)
+    if not EYEUPDATE[item.prefab] then
+        if Cur_Percent > cfgEyeItems then
+            return
+        end
+    end
+    if not DREADSTONE[item.prefab] then
+        if Cur_Percent > cfgDreadstone then
+            return
+        end
+    end
+
+    DebugPrint("Unequiping=" .. item.prefab .. " slot=" .. slot .. " at=" .. Cur_Percent)
+    if cfgForce then
+        item.unequiptask = item:DoPeriodicTask(0, function()
+            Unequip(item)
+        end)
+    end
+
+    Unequip(item)
+
+    if cfgNotif and G.ThePlayer.components.talker then
+        G.ThePlayer.components.talker:Say(notification:format(item.name or slot .. ' slot item'))
+    end
+
+end
+
+local function PostInit(inst)
+
+    local item = inst.entity:GetParent()
+
+    if item == nil or item.replica.equippable == nil then
+        return
+    end
+
+    inst:ListenForEvent('percentuseddirty', function()
+        AutoUnequip(inst)
+    end)
+
+end
+
+AddPrefabPostInit('inventoryitem_classified', function(inst)
+    if not G.TheNet:IsDedicated() then
+        inst:DoTaskInTime(0, function()
+            PostInit(inst)
+        end)
+    end
+end)
